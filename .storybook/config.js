e8560bf7a74ac8159a3913ebbf47eb9a92949d49
Maciek Kaczmarczyk <maciejkaczmarczyk95@gmail.com>
2018-04-10 18:22:10 +0200

    add intl

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,30 @@
-import { configure } from '@storybook/react';
+import { addDecorator, configure } from '@storybook/react';
+import { setIntlConfig, withIntl } from 'storybook-addon-intl';
+
+import { addLocaleData } from 'react-intl';
+import enLocaleData from 'react-intl/locale-data/en';
+
+addLocaleData(enLocaleData);
+
+const messages = {
+    en: {
+      'app.currencyItem.bid': 'Bid',
+      'app.currencyItem.ask': 'Ask',
+      'app.currencyItem.addToFav': 'Add to favourites',
+      'app.currencyItem.removeFromFav': 'Remove from favourites',
+      'app.currencyItem.follow': 'Add to favourite to follow rates',
+    },
+};
+
+const getMessages = (locale) => messages[locale];
+
+setIntlConfig({
+  locales: ['en'],
+  defaultLocale: 'en',
+  getMessages,
+});
+
+addDecorator(withIntl);
 
 const req = require.context('../src/components', true, /\.stories\.js$/)
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { IntlProvider } from 'react-intl';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <IntlProvider locale="en">
+    <App />
+  </IntlProvider>,
+  document.getElementById('root'),
+);
 
 registerServiceWorker();
